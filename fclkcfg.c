11463cf13cce48c76708c12199ba8fe5047e12aa
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2020-07-16 12:41:16 +0900

    [add] the function to change the resource clock dynamically.

--- fclkcfg.c
+++ fclkcfg.c
@@ -53,7 +53,7 @@ MODULE_DESCRIPTION("FPGA Clock Configuration Driver");
 MODULE_AUTHOR("ikwzm");
 MODULE_LICENSE("Dual BSD/GPL");
 
-#define DRIVER_VERSION     "1.6.0-rc.2"
+#define DRIVER_VERSION     "1.6.0-rc.3"
 #define DRIVER_NAME        "fclkcfg"
 #define DEVICE_MAX_NUM      32
 
@@ -111,19 +111,23 @@ MODULE_PARM_DESC(     debug_print , DRIVER_NAME " debug print enable");
 struct fclk_state {
     unsigned long        rate;
     bool                 enable;
+    unsigned long        resclk;
     bool                 rate_valid;
     bool                 enable_valid;
+    bool                 resclk_valid;
 };
 
 /**
- * of_get_fclk_state()  - get rate or enable property from device tree.
+ * of_get_fclk_state()  - get rate/enable/resource property from device tree.
+ *
  * @dev:         handle to the device structure.
  * @rate_name:   rate property name
  * @enable_name: enable property name
+ * @resclk_name: resource clock property name
  * @state:       address of fclk state data.
  * Return:       Success(=0) or error status(<0).
  */
-static void of_get_fclk_state(struct device* dev, const char* rate_name, const char* enable_name, struct fclk_state* state)
+static void of_get_fclk_state(struct device* dev, const char* rate_name, const char* enable_name, const char* resclk_name, struct fclk_state* state)
 {
     DEV_DBG(dev, "get %s start.\n", rate_name);
     {
@@ -157,6 +161,20 @@ static void of_get_fclk_state(struct device* dev, const char* rate_name, const c
         }
     }
     DEV_DBG(dev, "get %s done.\n", enable_name);
+
+    DEV_DBG(dev, "get %s start.\n", resclk_name);
+    {
+        int          retval;
+        unsigned int resclk;
+
+        retval = of_property_read_u32(dev->of_node, resclk_name, &resclk);
+
+        if (retval == 0) {
+            state->resclk_valid = true;
+            state->resclk       = resclk;
+        }
+    }
+    DEV_DBG(dev, "get %s done.\n", resclk_name);
 }
 
 /**
@@ -171,7 +189,9 @@ static void of_get_fclk_state(struct device* dev, const char* rate_name, const c
 struct fclk_device_data {
     struct device*       device;
     struct clk*          clk;
-    struct clk*          resource_clk;
+    struct clk**         resource_clks;
+    int                  resource_clks_size;
+    int                  resource_clk_id;
     dev_t                device_number;
     unsigned long        round_rate;
     struct fclk_state    insert;
@@ -183,9 +203,10 @@ struct fclk_device_data {
  *
  * This section defines the clock operation.
  *
- * * __fclk_set_enable()    - enable/disable clock.
- * * __fclk_set_rate()      - set clock rate.
- * * __fclk_change_state()  - change clock state.
+ * * __fclk_set_enable()       - enable/disable clock.
+ * * __fclk_set_rate()         - set clock rate.
+ * * __fclk_change_state()     - change clock state.
+ * * __fclk_change_resource()  - change resource clock.
  *
  */
 /**
@@ -242,6 +263,50 @@ static int __fclk_set_rate(struct fclk_device_data* this, unsigned long rate)
 }
 
 /**
+ * __fclk_change_resource() - change resource clock.
+ *
+ * @this:       Pointer to the fclk device data.
+ * @index:	index of resource_clks.
+ * Return:      Success(=0) or error status(<0).
+ *
+ */
+static int __fclk_change_resource(struct fclk_device_data* this, int index)
+{
+    struct device* dev = this-> device;
+
+    if ((this->resource_clks == NULL) && index == 0) {
+        this->resource_clk_id = index;
+        return 0;
+    }
+
+    if ((this->resource_clks != NULL) && (index >= 0) && (index < this->resource_clks_size)) {
+        struct clk*    resource_clk       = this->resource_clks[index];
+        int            set_parent_status  = 0;
+        bool           found_resource_clk = false;
+        struct clk*    curr_clk           = this->clk;
+        while (!IS_ERR_OR_NULL(curr_clk)) {
+            if (clk_has_parent(curr_clk, resource_clk) == true) {
+                found_resource_clk = true;
+                set_parent_status  = clk_set_parent(curr_clk, resource_clk);
+                break;
+            }
+            curr_clk = clk_get_parent(curr_clk);
+        }
+        if (set_parent_status != 0) {
+            dev_err(dev, "clk_set_parent(%s, %s) failed.\n" , __clk_get_name(curr_clk), __clk_get_name(resource_clk));
+            return set_parent_status;
+        }
+        if (found_resource_clk == false) {
+            dev_err(dev, "%s is not resource clock of %s.\n", __clk_get_name(resource_clk), __clk_get_name(this->clk));
+            return -EINVAL;
+        }
+        this->resource_clk_id = index;
+        return 0;
+    }
+    return -EINVAL;
+}
+
+/**
  * __fclk_change_state() - change clock state.
  *
  * @this:       Pointer to the fclk device data.
@@ -254,11 +319,17 @@ static int __fclk_change_state(struct fclk_device_data* this, struct fclk_state*
     int  retval      = 0;
     bool prev_enable = __clk_is_enabled(this->clk);
     bool next_enable = (next->enable_valid == true) ? next->enable : prev_enable;
+    bool next_resclk = ((next->resclk_valid == true) &&
+                        (next->resclk != this->resource_clk_id));
 
-    if ((next->rate_valid == true) && (prev_enable == true)) {
+    if (((next->rate_valid == true) || (next_resclk == true)) && (prev_enable == true)) {
         if (0 != (retval = __fclk_set_enable(this, false)))
             return retval;
         prev_enable = false;
+    }
+    if (next_resclk == true) {
+        if (0 != (retval = __fclk_change_resource(this, next->resclk)))
+            return retval;
     }
     if (next->rate_valid == true) {
         if (0 != (retval = __fclk_set_rate(this, next->rate)))
@@ -283,6 +354,8 @@ static int __fclk_change_state(struct fclk_device_data* this, struct fclk_state*
  * * /sys/class/udmabuf/<device-name>/enable
  * * /sys/class/udmabuf/<device-name>/rate
  * * /sys/class/udmabuf/<device-name>/round_rate
+ * * /sys/class/udmabuf/<device-name>/resource_clk
+ * * /sys/class/udmabuf/<device-name>/resource
  * * 
  */
 
@@ -347,6 +420,8 @@ static ssize_t fclk_set_rate(struct device *dev, struct device_attribute *attr, 
     next_state.rate_valid   = true;
     next_state.enable       = false;
     next_state.enable_valid = false;
+    next_state.resclk       = 0;
+    next_state.resclk_valid = false;
 
     if (0 != (set_result = __fclk_change_state(this, &next_state)))
         return (ssize_t)set_result;
@@ -381,11 +456,80 @@ static ssize_t fclk_set_round_rate(struct device *dev, struct device_attribute *
     return size;
 }
 
+/**
+ * fclk_set_resource()
+ */
+static ssize_t fclk_set_resource(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
+{
+    ssize_t                  get_result;
+    int                      set_result;
+    unsigned long            resclk;
+    struct fclk_state        next_state;
+    struct fclk_device_data* this = dev_get_drvdata(dev);
+
+    if (this->resource_clks == NULL)
+        return size;
+
+    if (0 != (get_result = kstrtoul(buf, 0, &resclk)))
+        return get_result;
+    
+    if (resclk >= this->resource_clks_size)
+        return -EINVAL;
+
+    next_state.rate         = 0;
+    next_state.rate_valid   = false;
+    next_state.enable       = false;
+    next_state.enable_valid = false;
+    next_state.resclk       = resclk;
+    next_state.resclk_valid = true;
+
+    if (0 != (set_result = __fclk_change_state(this, &next_state)))
+        return (ssize_t)set_result;
+
+    return size;
+}
+
+/**
+ * fclk_show_resource()
+ */
+static ssize_t fclk_show_resource(struct device *dev, struct device_attribute *attr, char *buf)
+{
+    struct fclk_device_data* this = dev_get_drvdata(dev);
+    return sprintf(buf, "%d\n", this->resource_clk_id);
+}
+
+/**
+ * fclk_show_resource_clks()
+ */
+static ssize_t fclk_show_resource_clks(struct device *dev, struct device_attribute *attr, char *buf)
+{
+    size_t                   size = 0;
+    struct fclk_device_data* this = dev_get_drvdata(dev);
+
+    if ((this->resource_clks != NULL) && (this->resource_clks_size > 0)) {
+        int    i;
+        char*  ptr  = buf;
+        for (i = 0; i < this->resource_clks_size; i++) {
+            struct clk* resource_clk = this->resource_clks[i];
+            size_t      len;
+            if (i == this->resource_clks_size-1)
+                len  = sprintf(ptr, "%s\n", __clk_get_name(resource_clk));
+            else
+                len  = sprintf(ptr, "%s, ", __clk_get_name(resource_clk));
+            ptr  += len;
+            size += len;
+        }
+    }
+    return size;
+}
+
 static struct device_attribute fclkcfg_device_attrs[] = {
   __ATTR(driver_version , 0444, fclk_show_driver_version, NULL               ),
   __ATTR(enable         , 0664, fclk_show_enable        , fclk_set_enable    ),
   __ATTR(rate           , 0664, fclk_show_rate          , fclk_set_rate      ),
   __ATTR(round_rate     , 0664, fclk_show_round_rate    , fclk_set_round_rate),
+  __ATTR(resource       , 0664, fclk_show_resource      , fclk_set_resource  ),
+  __ATTR(resource_clks  , 0444, fclk_show_resource_clks , NULL               ),
   __ATTR_NULL,
 };
 
@@ -395,6 +539,8 @@ static struct attribute *fclkcfg_attrs[] = {
   &(fclkcfg_device_attrs[1].attr),
   &(fclkcfg_device_attrs[2].attr),
   &(fclkcfg_device_attrs[3].attr),
+  &(fclkcfg_device_attrs[4].attr),
+  &(fclkcfg_device_attrs[5].attr),
   NULL
 };
 static struct attribute_group  fclkcfg_attr_group = {
@@ -434,18 +580,35 @@ static void fclk_device_info(struct fclk_device_data* this, struct platform_devi
 {
     struct device* dev = (pdev != NULL)? &pdev->dev : this->device;
 
+#define RES_INFO(dev, tag, resclk)                                                    \
+    if (this->resource_clks != NULL) {                                                \
+        if (((resclk) >= 0) && ((resclk) < this->resource_clks_size))                 \
+            dev_info(dev, tag "%s\n",  __clk_get_name(this->resource_clks[(resclk)]));\
+        else                                                                          \
+            dev_info(dev, tag "%d\n",  (int)(resclk));                                \
+    }
+
     dev_info(dev, "driver version : %s\n" , DRIVER_VERSION);
     dev_info(dev, "device name    : %s\n" , dev_name(this->device));
     dev_info(dev, "clock  name    : %s\n" , __clk_get_name(this->clk));
-    if (this->resource_clk != NULL)
-        dev_info(dev, "resource clock : %s\n", __clk_get_name(this->resource_clk));
     dev_info(dev, "clock  rate    : %lu\n", clk_get_rate(this->clk));
     dev_info(dev, "clock  enabled : %d\n" , __clk_is_enabled(this->clk));
-    if (this->remove.rate_valid == true)
-        dev_info(dev, "remove rate    : %lu\n", this->remove.rate);
-    if (this->remove.enable_valid == true)
-        dev_info(dev, "remove enable  : %d\n" , this->remove.enable);
-           
+    RES_INFO(dev, "resource clock : "     , this->resource_clk_id);
+    if ((this->resource_clks != NULL) && (this->resource_clks_size > 1)) {
+        int i;
+        for (i = 0; i < this->resource_clks_size; i++) {
+            struct clk* resource_clk = this->resource_clks[i];
+            dev_info(dev, "resource clocks: %d => %s"  , i, __clk_get_name(resource_clk));
+        }
+    }
+    {
+        if (this->remove.rate_valid   == true)
+            dev_info(dev, "remove rate    : %lu\n", this->remove.rate  );
+        if (this->remove.enable_valid == true)
+            dev_info(dev, "remove enable  : %d\n" , this->remove.enable);
+        if (this->remove.resclk_valid == true)
+            RES_INFO(dev, "remove resource: "     , this->remove.resclk);
+    }
 }
 
 /**
@@ -464,10 +627,17 @@ static int fclk_device_destroy(struct fclk_device_data* this)
         clk_put(this->clk);
         this->clk = NULL;
     }
-    if (this->resource_clk ) {
-        clk_put(this->resource_clk);
-        this->resource_clk = NULL;
+    if (this->resource_clks != NULL) {
+        int i;
+        for (i = 0 ; i < this->resource_clks_size ; i++) {
+            struct clk* resource_clk = this->resource_clks[i];
+            clk_put(resource_clk);
+        }
+        kfree(this->resource_clks);
+        this->resource_clks  = NULL;
     }
+    this->resource_clks_size = 0;
+    this->resource_clk_id    = 0;
     if (this->device       ) {
         device_destroy(fclkcfg_sys_class, this->device_number);
         this->device = NULL;
@@ -477,6 +647,30 @@ static int fclk_device_destroy(struct fclk_device_data* this)
         this->device_number = 0;
     }
     kfree(this);
+    return 0;
+}
+
+/**
+ * fclk_device_get_state_property() - get rate/enable/resource property from device.
+ *
+ * @this:        Pointer to the fclk device data.
+ * @dev:         handle to the device structure.
+ * @rate_name:   rate property name
+ * @enable_name: enable property name
+ * @resclk_name: resource clock property name
+ * @state:       address of fclk state data.
+ * Return:       Success(=0) or error status(<0).
+ *
+ */
+static int fclk_device_get_state_property(struct fclk_device_data* this, struct device* dev, const char* rate_name, const char* enable_name, const char* resclk_name, struct fclk_state* state)
+{
+    of_get_fclk_state(dev, rate_name, enable_name, resclk_name, state);
+    if ((this->resource_clks != NULL) &&
+        (state->resclk_valid == true) &&
+        (state->resclk >= this->resource_clks_size)) {
+        dev_err(dev, "invalid %s(=%lu).\n", resclk_name, state->resclk);
+        return -EINVAL;
+    }
     return 0;
 }
 
@@ -570,52 +764,75 @@ static struct fclk_device_data* fclk_device_create(struct device *dev)
     DEV_DBG(dev, "device_create done\n");
 
     /*
-     * get resource clock
+     * get resource clock list
      */
-    DEV_DBG(dev, "of_clk_get(1) start.\n");
+    DEV_DBG(dev, "of_clk_get(1..) start.\n");
     {
-        this->resource_clk = of_clk_get(dev->of_node, 1);
-        if (IS_ERR_OR_NULL(this->resource_clk)) {
-            this->resource_clk = NULL;
-        } else {
-            int         set_parent_status  = 0;
-            bool        found_resource_clk = false;
-            struct clk* curr_clk           = this->clk;
-            while (!IS_ERR_OR_NULL(curr_clk)) {
-                if (clk_has_parent(curr_clk, this->resource_clk) == true) {
-                    found_resource_clk = true;
-                    set_parent_status  = clk_set_parent(curr_clk, this->resource_clk);
-                    break;
+        int         clk_index;
+        struct clk* clk_list[32];
+        
+        for (clk_index = 0; clk_index < sizeof(clk_list); clk_index++) {
+            struct clk* resource_clk = of_clk_get(dev->of_node, clk_index+1);
+            if (IS_ERR_OR_NULL(resource_clk))
+                break;
+            clk_list[clk_index] = resource_clk;
+        }
+        this->resource_clks_size = clk_index;
+        if (this->resource_clks_size > 0) {
+            this->resource_clks = kzalloc(this->resource_clks_size*sizeof(struct clk*), GFP_KERNEL);
+            if (IS_ERR_OR_NULL(this->resource_clks)) {
+                dev_err(dev, "create resource_clks falied.\n");
+                for (clk_index = 0; clk_index < this->resource_clks_size; clk_index++) {
+                    clk_put(clk_list[clk_index]);
                 }
-                curr_clk = clk_get_parent(curr_clk);
-            }
-            if (set_parent_status != 0) {
-                dev_err(dev, "clk_set_parent(%s, %s) failed.\n" , __clk_get_name(curr_clk), __clk_get_name(this->resource_clk));
+                retval = PTR_ERR(this->resource_clks);
+                this->resource_clks      = NULL;
+                this->resource_clks_size = 0;
+                this->resource_clk_id    = 0;
                 goto failed;
+            } else {
+                for (clk_index = 0; clk_index < this->resource_clks_size; clk_index++) {
+                    this->resource_clks[clk_index] = clk_list[clk_index];
+                }
+                this->resource_clk_id   = -1;   /* Uninitialized resclk flag */
             }
-            if (found_resource_clk == false) {
-                dev_err(dev, "%s is not resource clock of %s.\n", __clk_get_name(this->resource_clk), __clk_get_name(this->clk));
-                goto failed;
-            }
+        } else {
+                this->resource_clks      = NULL;
+                this->resource_clks_size = 0;
+                this->resource_clk_id    = 0;
         }
     }
-    DEV_DBG(dev, "of_clk_get(1) done.\n");
+    DEV_DBG(dev, "of_clk_get(1..) done.\n");
+
     /*
      * get insert state
      */
-    of_get_fclk_state(dev, "insert-rate", "insert-enable", &this->insert);
-    
+    if (this->resource_clks != NULL) {
+        this->insert.resclk_valid = true; 
+        this->insert.resclk       = 0;
+    }
+    retval = fclk_device_get_state_property(this, dev, "insert-rate", "insert-enable", "insert-resource", &this->insert);
+    if (retval)
+        goto failed;
+
     /*
-     * change state
+     * change state to insert
      */
-    __fclk_change_state(this, &this->insert);
+    retval = __fclk_change_state(this, &this->insert);
+    if (retval) {
+        dev_err(dev, "fclk change state failed(%d).\n", retval);
+        goto failed;
+    }
     this->insert.enable = __clk_is_enabled(this->clk);
     this->insert.rate   = clk_get_rate(this->clk);
+    this->insert.resclk = this->resource_clk_id;
 
     /*
      * get remove state
      */
-    of_get_fclk_state(dev, "remove-rate", "remove-enable", &this->remove);
+    retval = fclk_device_get_state_property(this, dev, "remove-rate", "remove-enable", "remove-resource", &this->remove);
+    if (retval)
+        goto failed;
 
     return this;
 
